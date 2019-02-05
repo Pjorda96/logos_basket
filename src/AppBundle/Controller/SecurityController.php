c4fa50a92401084d67c52ee0bc555554e6122b01
PABLOPC\PabloPortatil <pjorda96@gmail.com>
2019-02-05 12:16:41 +0300

    security

--- src/AppBundle/Controller/SecurityController.php
+++ src/AppBundle/Controller/SecurityController.php
@@ -2,14 +2,16 @@
 
 namespace AppBundle\Controller;
 
-use AppBundle\Entity\User;
-use AppBundle\Form\UserType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
+/**
+ * Security controller.
+ *
+ * @Route("security")
+ */
 class SecurityController extends Controller
 {
     /**
